7b54767bcce90db268f2e5d9743736c38daae9d3
アンネ <krisjulom@gmail.com>
2025-12-11 16:34:47 +0800

    Add files via upload

---
+++ foreach_testing/foreach_testing/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] candy = { "chocolate", "snowbar", "mikmik", "stick-o" };
+
+            Array.Reverse(candy); //if reversed, don't place after foreach syntax
+            foreach (string candies in candy)
+            {
+                Console.WriteLine(candies);
+            }
+            Console.ReadLine();
+        }
+    }
+}
