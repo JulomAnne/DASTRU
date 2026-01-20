ad9bb18a0d68c1a49f83b73e2d3ef4ba40b13119
アンネ <krisjulom@gmail.com>
2026-01-20 13:26:49 +0800

    Add files via upload

---
+++ Single_Dimensional_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_Array
+{
+    class Program
+    {
+        //Kris Anne J. Ybañez
+        //ACTIVITY 2: Single Dimensional Array of Objects in OOP
+        static void Main(string[] args)
+        {
+            Songs[] songs = new Songs[4]; //Declare array
+
+            //Assign values for each object
+            songs[0] = new Songs("I'm Back", "Royal Deluxe", "Epic Rock");
+            songs[1] = new Songs("I Only Paint in Red Now", "Lydia The Bard", "Pop");
+            songs[2] = new Music("Beat It", "Michael Jackson", "Hard Rock", 150);
+            songs[3] = new Performance("Dangerous", "Jorge Rivera Herrans", "New jack swing pop R&B", 10);
+
+            //Display information in each song
+            foreach(Songs song in songs)
+            {
+                song.DisplayInfo();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Single_Dimensional_Array/Songs.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Dimensional_Array
+{
+    public class Songs
+    {
+        //Declare properties
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+
+        //Parameterized Constructor
+        public Songs(string title, string artist, string genre)
+        {
+            Title = title;
+            Artist = artist;
+            Genre = genre;
+        }
+
+        //Virtual method to be overriden
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("\n----------| List |----------");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Artist: {Artist}");
+            Console.WriteLine($"Genre: {Genre}");
+        }
+    }
+
+    public class Music : Songs
+    {
+        public double Releases { get; set; } //Additional property
+
+        //Paramterized Constructor
+        public Music(string title, string artist, string genre, double releases) : base(title, artist, genre)
+        {
+            Releases = releases;
+        }
+
+        //Override Method to display
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Releases: {Releases}");
+        }
+    }
+
+    public class Performance : Songs
+    {
+        public double Album { get; set; } //Additional property
+
+        //Paramterized Constructor
+        public Performance(string title, string artist, string genre, double album) : base(title, artist, genre)
+        {
+            Album = album;
+        }
+
+        //Override Method to display
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine($"Album: {Album}");
+        }
+    }
+}
