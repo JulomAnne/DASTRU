157d9315399e77d586665d2083acb1e8c2cb4b72
アンネ <krisjulom@gmail.com>
2026-03-03 16:18:59 +0800

    Add files via upload

---
+++ Simple_LinkedList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+
+            list.AddFirst("Janine");
+            list.AddBefore(list.First, "Carmel");
+            list.AddFirst("Gemma");
+            list.AddAfter(list.First, "Preselle");
+            list.AddLast("Pinky Rose");
+
+            foreach (string names in list)
+            {
+                Console.WriteLine(names);
+            }
+            Console.ReadLine();
+        }
+    }
+}
